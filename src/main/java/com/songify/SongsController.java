aaacc6c8dfa3f25e154a15a9ac11206b7ff75a16
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-09-12 11:44:27 +0200

    Add `limit` query parameter to GET /songs endpoint

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -3,19 +3,29 @@ package com.songify;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class SongsController {
 
-    Map<Integer, String> dataBase = new HashMap<>(Map.of(1, "ShawnMendes songs1", 2, "ArianaGrande songs2"));
+    Map<Integer, String> dataBase = new HashMap<>(Map.of(1, "ShawnMendes songs1", 2, "ArianaGrande songs2", 3, "Crimsonsun song3", 4, "Letdown"));
 
     @GetMapping("/songs")
-    public ResponseEntity<SongsResponseDto> getAllSongs() {
+    public ResponseEntity<SongsResponseDto> getAllSongs(@RequestParam(required = false) Integer limit) {
+        if (limit != null) {
+            Map<Integer, String> limitedMap = dataBase.entrySet()
+                    .stream()
+                    .limit(limit)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            SongsResponseDto response = new SongsResponseDto(limitedMap);
+            return ResponseEntity.ok(response);
+        }
         SongsResponseDto response = new SongsResponseDto(dataBase);
         return ResponseEntity.ok(response);
     }
