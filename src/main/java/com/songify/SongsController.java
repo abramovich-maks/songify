b9c2b255facf31b92a3b2be73d5f3ee0a4182386
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-09-12 11:25:03 +0200

    Add GET /songs/{id} endpoint to fetch song by ID

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -2,16 +2,31 @@ package com.songify;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.HashMap;
+
+import java.util.Map;
 
 @RestController
 public class SongsController {
 
+    Map<Integer, String> dataBase = new HashMap<>(Map.of(1, "ShawnMendes songs1", 2, "ArianaGrande songs2"));
+
     @GetMapping("/songs")
-    public ResponseEntity<SongsResponseDto> getAllSongs(){
-        SongsResponseDto response = new SongsResponseDto(List.of("ShawnMendes songs1", "ArianaGrande songs2"));
+    public ResponseEntity<SongsResponseDto> getAllSongs() {
+        SongsResponseDto response = new SongsResponseDto(dataBase);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/songs/{id}")
+    public ResponseEntity<SingleSongResponseDto> getSongsById(@PathVariable Integer id) {
+        String song = dataBase.get(id);
+        if (song == null) {
+            return ResponseEntity.notFound().build();
+        }
+        SingleSongResponseDto response = new SingleSongResponseDto(song);
         return ResponseEntity.ok(response);
     }
 
