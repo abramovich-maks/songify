1f7e7ebda094a006246ac88f749f96717c36983a
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-09-21 16:05:53 +0200

    Convert `SongEntity` from record to class and annotate as JPA entity for database management

--- src/main/java/com/songify/song/domain/model/SongEntity.java
+++ src/main/java/com/songify/song/domain/model/SongEntity.java
@@ -1,7 +1,37 @@
 package com.songify.song.domain.model;
 
-import lombok.Builder;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Builder
-public record SongEntity(String song, String artist) {
+@Entity
+@Getter
+@Setter
+@Table(name = "song")
+
+public class SongEntity {
+
+    public SongEntity() {
+    }
+
+    public SongEntity(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+    }
+
+    public SongEntity(Long id, String name, String artist) {
+        this.id = id;
+        this.name = name;
+        this.artist = artist;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    public String name;
+
+    @Column(nullable = false)
+    public String artist;
 }
