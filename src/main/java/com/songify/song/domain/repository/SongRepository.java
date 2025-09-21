a2afb47d38a77e4e4f0b015bfeae0ec7052c8b70
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-09-21 17:07:28 +0200

    Add `findById` method to `SongRepository` interface

--- src/main/java/com/songify/song/domain/repository/SongRepository.java
+++ src/main/java/com/songify/song/domain/repository/SongRepository.java
@@ -4,10 +4,13 @@ import com.songify.song.domain.model.SongEntity;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SongRepository extends Repository<SongEntity, Long> {
 
     SongEntity save(SongEntity song);
 
     List<SongEntity> findAll();
+
+    Optional<SongEntity> findById(Long id);
 }
