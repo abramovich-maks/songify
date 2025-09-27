5bcec9b05186dd980c9103ad1fd2f74be6792a60
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-09-27 18:48:42 +0200

    Migrate to Flyway for database versioning

--- src/main/resources/db.migration/V1__crerate_song_table.sql
+++ src/main/resources/db.migration/V1__crerate_song_table.sql
@@ -1,17 +1,9 @@
--- CREATE TABLE IF NOT EXISTS SONG
--- (
---     ID     BIGSERIAL PRIMARY KEY,
---     ARTIST VARCHAR(255) NOT NULL,
---     NAME   VARCHAR(255) NOT NULL
--- );
-
 CREATE TABLE song
 (
-    id           BIGSERIAL PRIMARY KEY,
+    id           BIGSERIAL PRIMARY KEY ,
     name         VARCHAR(255) NOT NULL,
     artist       VARCHAR(255) NOT NULL,
     release_date TIMESTAMP WITHOUT TIME ZONE,
     duration     BIGINT,
     language     VARCHAR(255)
 );
-
