597df906bd084070c9e30a87c02bd82cdf7a0484
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-09-30 17:38:07 +0200

    Refactor migration scripts

--- src/main/resources/db.migration/V4__create_table_genre.sql
+++ src/main/resources/db.migration/V4__create_table_genre.sql
@@ -1,7 +1,5 @@
 CREATE TABLE genre
 (
-    id         BIGSERIAL PRIMARY KEY,
-    name       VARCHAR(255) NOT NULL UNIQUE,
-    created_on TIMESTAMP WITHOUT TIME ZONE,
-    uuid       UUID
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
 );
