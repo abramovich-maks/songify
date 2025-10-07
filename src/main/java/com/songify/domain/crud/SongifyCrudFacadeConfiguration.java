2bbd4bb049e739fe167e595ff0a263f8d2550c08
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-10-07 11:46:31 +0200

    Refactor CRUD operations to centralize validations in respective service classes

---
+++ src/main/java/com/songify/domain/crud/SongifyCrudFacadeConfiguration.java
@@ -0,0 +1,46 @@
+package com.songify.domain.crud;
+
+class SongifyCrudFacadeConfiguration {
+
+    public static SongifyCrudFasade createSongifyCrud(final SongRepository songRepository,
+                                                      final GenreRepository genreRepository,
+                                                      final ArtistRepository artistRepository,
+                                                      final AlbumRepository albumRepository) {
+        SongRetriever songRetriever = new SongRetriever(songRepository);
+        GenreRetriever genreRetriever = new GenreRetriever(genreRepository);
+        AlbumRetriever albumRetriever = new AlbumRetriever(albumRepository);
+        ArtistRetriever artistRetriever = new ArtistRetriever(artistRepository);
+        SongAdder songAdder = new SongAdder(songRepository, genreRetriever, artistRetriever, albumRetriever);
+        SongDeleter songDeleter = new SongDeleter(songRepository,songRetriever);
+        SongUpdater songUpdater = new SongUpdater(songRepository, songRetriever, genreRetriever, albumRetriever, artistRetriever);
+        SongAssigner songAssigner = new SongAssigner(songRetriever, artistRetriever, albumRetriever, genreRetriever);
+        GenreAdder genreAdder = new GenreAdder(genreRepository);
+        GenreUpdater genreUpdater = new GenreUpdater(genreRetriever);
+        AlbumAdder albumAdder = new AlbumAdder(songRetriever, albumRepository);
+        AlbumDeleter albumDeleter = new AlbumDeleter(albumRepository,albumRetriever);
+        AlbumUpdater albumUpdater = new AlbumUpdater(albumRetriever, artistRetriever, songRetriever);
+        ArtistAdder artistAdder = new ArtistAdder(artistRepository, albumAdder, songAdder);
+        ArtisDeleter artisDeleter = new ArtisDeleter(artistRepository, artistRetriever, albumDeleter, albumRetriever, songDeleter);
+        ArtistAssigner artistAssigner = new ArtistAssigner(artistRetriever, albumRetriever);
+        ArtistUpdater artistUpdater = new ArtistUpdater(artistRetriever);
+        return new SongifyCrudFasade(
+                songAdder,
+                songRetriever,
+                songDeleter,
+                songUpdater,
+                songAssigner,
+                genreAdder,
+                genreRetriever,
+                genreUpdater,
+                albumAdder,
+                albumRetriever,
+                albumDeleter,
+                albumUpdater,
+                artistAdder,
+                artistRetriever,
+                artisDeleter,
+                artistAssigner,
+                artistUpdater
+        );
+    }
+}
