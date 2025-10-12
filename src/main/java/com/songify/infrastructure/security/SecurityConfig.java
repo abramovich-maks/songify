51cd4d69df049ca1111ea155d661a140d03c6e3c
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-10-12 17:09:46 +0200

    Add `SecurityConfig` with in-memory users and no-op password encoder

---
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.songify.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var manager = new InMemoryUserDetailsManager();
+        var user = User.withUsername("maksim")
+                .password("12345")
+                .roles("USER")
+                .build();
+
+        var user2 = User.withUsername("albert")
+                .password("12345")
+                .roles("USER", "ADMIN")
+                .build();
+
+        manager.createUser(user);
+        manager.createUser(user2);
+        return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
