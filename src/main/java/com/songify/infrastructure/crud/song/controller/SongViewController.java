6f3802e75d0753a33d4dddb9d46d297eff7aaa0a
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-10-12 13:56:49 +0200

    Add song details view and update song gallery with details link

--- src/main/java/com/songify/infrastructure/crud/song/controller/SongViewController.java
+++ src/main/java/com/songify/infrastructure/crud/song/controller/SongViewController.java
@@ -1,18 +1,22 @@
 package com.songify.infrastructure.crud.song.controller;
 
 import com.songify.domain.crud.SongifyCrudFacade;
+import com.songify.domain.crud.dto.GetSongDto;
 import com.songify.domain.crud.dto.SongDto;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class SongViewController {
 
-    SongifyCrudFacade songifyCrudFacade;
+    private final SongifyCrudFacade songifyCrudFacade;
 
 
     @GetMapping("/")
@@ -25,5 +29,12 @@ public class SongViewController {
         List<SongDto> songs = songifyCrudFacade.findAllSongs(Pageable.unpaged());
         model.addAttribute("songs", songs);
         return "song";
+    }
+
+    @GetMapping("/view/songs/{songId}")
+    public String songById(Model model, @PathVariable Long songId) {
+        GetSongDto songDtoById = songifyCrudFacade.findSongDtoById(songId);
+        model.addAttribute("songs", songDtoById);
+        return "songById";
     }
 }
