8f05713d70c1779f21a409fc487afe6b18afdd14
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-10-13 13:20:28 +0200

    add `SecurityFilterChain` bean

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.songify.infrastructure.security;
 import com.songify.domain.usercrud.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 class SecurityConfig {
@@ -18,5 +21,14 @@ class SecurityConfig {
     @Bean
     public UserDetailsManager userDetailsService(UserRepository userRepository) {
         return new UserDetailsServiceImpl(userRepository, passwordEncoder());
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(c -> c.disable())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
